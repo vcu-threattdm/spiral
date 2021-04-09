7b254231995f4432d364a9c5b673e79b8a4f0e64
vcu-threattdm <78376316+vcu-threattdm@users.noreply.github.com>
2021-04-09 19:39:17 -0400

    Update homework.py

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,4 +1,13 @@
+diag = clock
 def spiralize(number):
+    number*=number
+    start=1
+    inc=2
     return_value = 1
-
+    while start<number:
+        for i in range(4):
+            start+=inc
+            return_value+=start
+        inc+=2
     return return_value
+print (spiralize(501))
